324f18363164e16b7d674b8e0affc8371a0fa286
Awungia112 <awungiaf@gmail.com>
2025-05-14 09:52:45 +0100

    edited the function

--- 1expanding_cards/script.js
+++ 1expanding_cards/script.js
@@ -1,18 +1,14 @@
-const panels = document.querySelectorAll(".panel");
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
 
 function removeActiveClasses() {
-  panels.forEach((panel) => {
-    panel.classList.remove("active");
-  });
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
 }
-
-panels.forEach((panel) => {
-  panel.addEventListener("click", () => {
-    if (panel.classList.contains("active")) {
-      removeActiveClasses();
-    } else {
-      removeActiveClasses();
-      panel.classList.add("active");
-    }
-  });
-});
